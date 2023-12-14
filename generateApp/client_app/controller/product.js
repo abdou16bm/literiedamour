09baa81a4129e89ea6a1d3d8176287333f7fe095
Abderrahmane Bouchama <abdou16bm@yahoo.fr>
2023-12-14 12:36:17 +0100

    update categorie filter client

--- generateApp/client_app/controller/product.js
+++ generateApp/client_app/controller/product.js
@@ -5,12 +5,13 @@
 const product_module = require ("../../admin_app/lib/product")
 const product_details_module = require ("../../admin_app/lib/product_details")
 const brand_module = require ("../../admin_app/lib/brand")
+const category_module = require ("../../admin_app/lib/category")
 const sub_category_module = require ("../../admin_app/lib/sub_category")
 
 
- 
+
 const product_details = function (req, res) {
-    
+
     const id = req.params.id
 
     product_module.product_get_one_client(id,function (err,result1) {
@@ -21,14 +22,14 @@ const product_details = function (req, res) {
 
             if (err) console.log(err)
 
-            
+
             res.render('product_details',{product : result1, product_details : result2, session : req.session, err : err});
 
-            
+
         })
-        
+
     })
-  
+
 }
 
 
@@ -42,180 +43,190 @@ const product_list_page = function (req,res) {
 
     let urlQuery = ""
 
-     if ( typeof(req.url.split("?")[1]) != "undefined" && req.url.split("?")[1] != null && req.url.split("?")[1] != "") {
+    if ( typeof(req.url.split("?")[1]) != "undefined" && req.url.split("?")[1] != null && req.url.split("?")[1] != "") {
 
-       urlQuery = "?"+req.url.split("?")[1]
+        urlQuery = "?"+req.url.split("?")[1]
 
-     }
+    }
 
 
-     if (typeof(req.query.cat) != "undefined" && req.query.cat != null && req.query.cat != "") {
+    if (typeof(req.query.catP) != "undefined" && req.query.catP != null && req.query.catP != "") {
 
-          filterObj.cat = req.query.cat
+        filterObj.catP = req.query.catP
 
-     }
-     if (typeof(req.query.bprice) != "undefined" && req.query.bprice != null && req.query.bprice != "") {
+    }
+    if (typeof(req.query.cat) != "undefined" && req.query.cat != null && req.query.cat != "") {
 
-          filterObj.bprice = req.query.bprice
-     }
-     if (typeof(req.query.eprice) != "undefined" && req.query.eprice != null && req.query.eprice != "") {
+        filterObj.cat = req.query.cat
 
-          filterObj.eprice = req.query.eprice
-     }
-     if (typeof(req.query.brand) != "undefined" && req.query.brand != null && req.query.brand != "") {
+    }
+    if (typeof(req.query.bprice) != "undefined" && req.query.bprice != null && req.query.bprice != "") {
 
-          filterObj.brand = req.query.brand
+        filterObj.bprice = req.query.bprice
+    }
+    if (typeof(req.query.eprice) != "undefined" && req.query.eprice != null && req.query.eprice != "") {
 
-     }
-     if (typeof(req.query.product) != "undefined" && req.query.product != null && req.query.product != "") {
+        filterObj.eprice = req.query.eprice
+    }
+    if (typeof(req.query.brand) != "undefined" && req.query.brand != null && req.query.brand != "") {
 
-          filterObj.product = req.query.product
+        filterObj.brand = req.query.brand
 
-     }
+    }
+    if (typeof(req.query.product) != "undefined" && req.query.product != null && req.query.product != "") {
 
-     brand_module.brand_get_all_client(function (err,result1) {
+        filterObj.product = req.query.product
 
-          if (err) console.log(err)
+    }
 
-          sub_category_module.sub_category_get_all_client(function (err,result2) {
+    brand_module.brand_get_all_client(function (err,result1) {
 
-               if (err) console.log(err)
+        if (err) console.log(err)
 
-               //console.log("filter : ",filterObj)
-               product_module.product_count_page_filter(filterObj,limit_page,function (err,result3) {
+        category_module.category_get_all(function (err,result5) {
+
+            if (err) console.log(err)
+
+            sub_category_module.sub_category_get_all_client(function (err,result2) {
+
+                if (err) console.log(err)
+
+                //console.log("filter : ",filterObj)
+                product_module.product_count_page_filter(filterObj,limit_page,function (err,result3) {
                     if (err) console.log('err : ',err) ;
                     if (Array.isArray(result3) == true && result3.length)
                     {
-                         if (result3[0].count_p > 0) count_page = result3[0].count_p;
-                         product_module.product_get_all_limit_filter(filterObj,current_page*limit_page,limit_page,'table_pk','DESC',function (err,result4) {
-                              
-                              if (err) console.log('error',err);
+                        if (result3[0].count_p > 0) count_page = result3[0].count_p;
+                        product_module.product_get_all_limit_filter(filterObj,current_page*limit_page,limit_page,'table_pk','DESC',function (err,result4) {
 
-                              if(req.baseUrl == "/api") {
-                              res.send({products : result4, brands : result1, sub_category : result2, urlQuery: urlQuery, info:{count_page : count_page, limit_page:limit_page, current_page : parseInt(current_page)+1}, err : err, session : req.session});
-                                   } else {
-                              res.render('products_list',{products : result4, brands : result1, sub_category : result2, urlQuery: urlQuery, info:{count_page : count_page, limit_page:limit_page, current_page : parseInt(current_page)+1}, err : err, session : req.session});
-                              }        
-                         
-                         })
+                            if (err) console.log('error',err);
+
+                            if(req.baseUrl == "/api") {
+                                res.send({products : result4, brands : result1, category : result5, sub_category : result2, urlQuery: urlQuery, info:{count_page : count_page, limit_page:limit_page, current_page : parseInt(current_page)+1}, err : err, session : req.session});
+                            } else {
+                                res.render('products_list',{products : result4, brands : result1, category : result5, sub_category : result2, urlQuery: urlQuery, info:{count_page : count_page, limit_page:limit_page, current_page : parseInt(current_page)+1}, err : err, session : req.session});
+                            }
+
+                        })
                     }
                     else {
-                         if(req.baseUrl == "/api") {
-                    res.send({products:[], brands : result1, sub_category : result2, urlQuery: urlQuery, info:{count_page : 0, limit_page:limit_page, current_page : parseInt(current_page)+1}, err : err, session : req.session})
-                         } else {
-                    res.render('products_list',{products : [], brands : result1, sub_category : result2, urlQuery: urlQuery, info:{count_page : count_page, limit_page:limit_page, current_page : parseInt(current_page)+1}, err : err, session : req.session});
-                         }
+                        if(req.baseUrl == "/api") {
+                            res.send({products:[], brands : result1, category : result5, sub_category : result2, urlQuery: urlQuery, info:{count_page : 0, limit_page:limit_page, current_page : parseInt(current_page)+1}, err : err, session : req.session})
+                        } else {
+                            res.render('products_list',{products : [], brands : result1, category : result5, sub_category : result2, urlQuery: urlQuery, info:{count_page : count_page, limit_page:limit_page, current_page : parseInt(current_page)+1}, err : err, session : req.session});
+                        }
                     }
-               })
-          
-          })
-          
-     }) 
-               
+                })
+            })
+
+        })
+
+    })
+
 };
-    
-    
+
+
 
 
 const product_compare_choice = function (req, res) {
-    
-     let product1 = req.params.p1
 
- 
-     product_module.product_get_one_client(product1,function (err,result1) {
- 
-          if (err) console.log(err)
+    let product1 = req.params.p1
 
-          if (result1.length > 0) {
 
-               product_details_module.product_details_get_all_product_client(product1,function (err,resul2) {
+    product_module.product_get_one_client(product1,function (err,result1) {
+
+        if (err) console.log(err)
+
+        if (result1.length > 0) {
+
+            product_details_module.product_details_get_all_product_client(product1,function (err,resul2) {
+
+                if (err) console.log(err)
+
+
+                product_module.product_get_all_client(result1[0].sub_cat_id,function (err,resul3) {
 
                     if (err) console.log(err)
 
+                    brand_module.brand_get_all_client(function (err,result4) {
 
-                    product_module.product_get_all_client(result1[0].sub_cat_id,function (err,resul3) {
-               
-                         if (err) console.log(err)
+                        if (err) console.log(err)
 
-                         brand_module.brand_get_all_client(function (err,result4) {
 
-                              if (err) console.log(err)
+                        res.render('products_compare_choice',{product1 : result1, product1_details : resul2, products : resul3, brands : result4, session : req.session, err : err});
 
-                              
-                              res.render('products_compare_choice',{product1 : result1, product1_details : resul2, products : resul3, brands : result4, session : req.session, err : err});
-                         
-                         
-                         })
 
                     })
-               
-               })
-          
-          } else {
 
-               res.render('products_compare_choice',{product1 : [], product1_details : [], products : [], brands : [], session : req.session, err : err});
+                })
 
-          }
-            
-     })
-   
- }
+            })
+
+        } else {
+
+            res.render('products_compare_choice',{product1 : [], product1_details : [], products : [], brands : [], session : req.session, err : err});
+
+        }
+
+    })
+
+}
 
 
 
- const product_compare = function (req, res) {
-    
-     const product1 = req.params.p1
-     const product2 = req.params.p2
+const product_compare = function (req, res) {
 
-     if (typeof(product1) != "undefined" && product1 != null && product1 != "" && typeof(product2) != "undefined" && product2 != null && product2 != "") {
-    
-          if (product1 != product2) {
+    const product1 = req.params.p1
+    const product2 = req.params.p2
 
-               product_module.product_get_one_client(product1,function (err,result1) {
-     
+    if (typeof(product1) != "undefined" && product1 != null && product1 != "" && typeof(product2) != "undefined" && product2 != null && product2 != "") {
+
+        if (product1 != product2) {
+
+            product_module.product_get_one_client(product1,function (err,result1) {
+
+                if (err) console.log(err)
+
+                product_details_module.product_details_get_all_product_client(product1,function (err,resul2) {
+
                     if (err) console.log(err)
-          
-                    product_details_module.product_details_get_all_product_client(product1,function (err,resul2) {
-          
-                         if (err) console.log(err)
-          
-                         product_module.product_get_one_client(product2,function (err,result3) {
-          
-                              if (err) console.log(err)
-                    
-                              product_details_module.product_details_get_all_product_client(product2,function (err,result4) {
-                    
-                                   if (err) console.log(err)
-          
-                                   res.render('products_compare',{product1 : result1, product1_details : resul2, product2 : result3, product2_details : result4, session : req.session, err : err});
-                              
-                              })
-                                   
-                                   
-                         })
-                    
+
+                    product_module.product_get_one_client(product2,function (err,result3) {
+
+                        if (err) console.log(err)
+
+                        product_details_module.product_details_get_all_product_client(product2,function (err,result4) {
+
+                            if (err) console.log(err)
+
+                            res.render('products_compare',{product1 : result1, product1_details : resul2, product2 : result3, product2_details : result4, session : req.session, err : err});
+
+                        })
+
+
                     })
-                    
-               })
-               
-          } else {
 
-               res.render('products_compare',{product1 : [], product1_details : [], product2 : [], product2_details : [], sameProduct : true, session : req.session, err : err});
+                })
 
-          }
+            })
 
-         
+        } else {
 
-     } else {
+            res.render('products_compare',{product1 : [], product1_details : [], product2 : [], product2_details : [], sameProduct : true, session : req.session, err : err});
 
-          res.render('products_compare',{product1 : [], product1_details : [], product2 : [], product2_details : [], session : req.session, err : err});
+        }
 
-     }
 
-   
- }
-    
+
+    } else {
+
+        res.render('products_compare',{product1 : [], product1_details : [], product2 : [], product2_details : [], session : req.session, err : err});
+
+    }
+
+
+}
+
 
 module.exports = {
 
--- generateApp/client_app/public/lib/multi_filter.js
+++ generateApp/client_app/public/lib/multi_filter.js
@@ -1,3 +1,4 @@
+let cat = []
 let sub_cat = []
 let brands = []
 let price = []
@@ -6,41 +7,41 @@ let eprice = ""
 const priceMin = document.getElementById("price-min")
 const priceMax = document.getElementById("price-max")
 const catDiv = document.getElementById("cat-choice")
+const subCatDiv = document.getElementById("subCat-choice")
 const inputCatList = catDiv.querySelectorAll("input")
+const inputSubCatList = subCatDiv.querySelectorAll("input")
 const brandDiv = document.getElementById("brand-choice")
 const inputBrandList = brandDiv.querySelectorAll("input")
 
 
 
 if (document.getElementById('price-min') && document.getElementById('price-max')) {
-    
+
     document.getElementById('price-min').oninput = () => {
 
         if (priceMin.value == 0) {
-        
+
             setTimeout(() => {
-                priceMin.value = "" 
+                priceMin.value = ""
             }, 100);
-      
+
         }
-    
+
     }
 
     document.getElementById('price-max').oninput = () => {
 
         if (priceMax.value == 0) {
-        
+
             setTimeout(() => {
-                priceMax.value = "" 
+                priceMax.value = ""
             }, 100);
-      
+
         }
-    
+
     }
 
 }
-
-
 
 if (document.getElementById('cat-choice')) {
 
@@ -48,65 +49,99 @@ if (document.getElementById('cat-choice')) {
 
 
         for (let i = 0; i < inputCatList.length; i++) {
-           
+
             if (inputCatList[i].checked == true) {
-    
-                if (sub_cat.indexOf(inputCatList[i].value) == -1) {
-    
-                    sub_cat.push(inputCatList[i].value)
-                    
+
+                if (cat.indexOf(inputCatList[i].value) == -1) {
+
+                    cat.push(inputCatList[i].value)
+
                 }
-                
+
             } else {
-    
-                if (sub_cat.indexOf(inputCatList[i].value) != -1) {
-    
-                    sub_cat.splice(sub_cat.indexOf(inputCatList[i].value),1)
-                    
+
+                if (cat.indexOf(inputCatList[i].value) != -1) {
+
+                    cat.splice(cat.indexOf(inputCatList[i].value),1)
+
                 }
-    
+
             }
-            
+
         }
-    
-    
-        //console.log("subCat : ",sub_cat)
-    
+
+
+        console.log("cat : ",cat)
+
     }
-    
+
+}
+
+if (document.getElementById('subCat-choice')) {
+
+    document.getElementById('subCat-choice').onclick = () => {
+
+
+        for (let i = 0; i < inputCatList.length; i++) {
+
+            if (inputSubCatList[i].checked == true) {
+
+                if (sub_cat.indexOf(inputSubCatList[i].value) == -1) {
+
+                    sub_cat.push(inputSubCatList[i].value)
+
+                }
+
+            } else {
+
+                if (sub_cat.indexOf(inputSubCatList[i].value) != -1) {
+
+                    sub_cat.splice(sub_cat.indexOf(inputSubCatList[i].value),1)
+
+                }
+
+            }
+
+        }
+
+
+        console.log("subCat : ",sub_cat)
+
+    }
+
 }
 
 
 
 if (document.getElementById('brand-choice')) {
-    
+
     document.getElementById('brand-choice').onclick = () => {
 
         for (let i = 0; i < inputBrandList.length; i++) {
-           
+
             if (inputBrandList[i].checked == true) {
-    
+
                 if (brands.indexOf(inputBrandList[i].value) == -1) {
-    
+
                     brands.push(inputBrandList[i].value)
-                    
+
                 }
-                
+
             } else {
-    
+
                 if (brands.indexOf(inputBrandList[i].value) != -1) {
-    
+
                     brands.splice(brands.indexOf(inputBrandList[i].value),1)
-                    
+
                 }
-    
+
             }
-            
+
         }
-    
-    
+
+
         //console.log("brands : ",brands)
-    
+
     }
 
 }
@@ -118,29 +153,29 @@ if (document.getElementById('btn-filter')) {
     document.getElementById('btn-filter').onclick = () => {
 
         if (priceMin.value != "" && priceMin.value != 0 && priceMax.value != "" && priceMax.value != 0) {
-            
+
             price.push(priceMin.value)
             price.push(priceMax.value)
             bprice = priceMin.value
             eprice = priceMax.value
-    
+
         } else {price = []}
-    
-        if (sub_cat.length > 0 || brands.length > 0 || price.length > 0 ) {
-           
-      
-            location.href = "/products/list/1?cat="+sub_cat+"&bprice="+bprice+"&eprice="+eprice+"&brand="+brands
-    
-    
+
+        if (cat.length > 0 || sub_cat.length > 0 || brands.length > 0 || price.length > 0 ) {
+
+
+            location.href = "/products/list/1?catP="+cat+"&cat="+sub_cat+"&bprice="+bprice+"&eprice="+eprice+"&brand="+brands
+
+
         } else {
-    
-            document.getElementById("alert_msg").innerHTML = 
+
+            document.getElementById("alert_msg").innerHTML =
             "<div class='alert alert-danger' role='alert'>Manque d'informations ! </div>"
             $('#alert_modal').modal('show');
-    
+
         }
-    
+
     }
-    
+
 }
 
