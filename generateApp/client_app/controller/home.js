9709d3bd9a6757563dc10c81354027bf17c160ee
Abderrahmane Bouchama <abdou16bm@yahoo.fr>
2023-12-24 11:26:47 +0100

    update client banner dynamic

--- generateApp/client_app/controller/home.js
+++ generateApp/client_app/controller/home.js
@@ -13,14 +13,14 @@ const home = function (req, res) {
     let dataBanner = {}
 
     if (fs.existsSync('./admin_app/public/img/banner/data.ini')) {
-        
-        file_content = fs.readFileSync('./admin_app/public/img/banner/data.ini',{encoding : 'utf-8'})
+
+        let file_content = fs.readFileSync('./admin_app/public/img/banner/data.ini',{encoding : 'utf-8'})
         file_content = JSON.parse(file_content);
 
         console.log("file banner content : ",file_content)
 
         if (typeof(file_content.text) != 'undefined') dataBanner = file_content
-    
+
     }
 
 
@@ -29,15 +29,15 @@ const home = function (req, res) {
         if (err) console.log(err)
 
         for (let i = 0; i < result1.length; i++) {
-            
+
             product_module.product_get_all_client(result1[i].sub_cat_id,function (err,subcat_products) {
 
                 if (err) console.log(err)
 
                 result1[i].products = subcat_products
-            
+
             })
-            
+
         }
 
         product_module.product_get_all_top(12,function (err,result2) {
@@ -45,13 +45,13 @@ const home = function (req, res) {
             if (err) console.log(err)
             //console.log(result1)
             res.render('home',{product_top : result2, sub_category : result1, banner : dataBanner, err : err, session : req.session});
-            
-    
+
+
         })
-    
-    
+
+
     })
-        
+
 
 }
 
