af462f4d2a8702aca2abbc1bd6dd92699ee81148
Ghano <ghanoatm95@gmail.com>
2024-01-30 14:46:03 +0100

    update db/ add delivery_type table

--- database/update.sql
+++ database/update.sql
@@ -13,3 +13,53 @@ SET stat_color = 'success' WHERE stat_id = 4;
 
 INSERT INTO `status` (`stat_id`, `stat_name`, `stat_color`) VALUES
 (5, 'Retour', 'secondary');
+
+
+/* ------------------------------------add delivery type ------------------------------------*/
+
+-- Listage de la structure de la table ecom_literiedamour. delivery_type
+CREATE TABLE IF NOT EXISTS `delivery_type` (
+  `delivery_type_id` int(11) NOT NULL AUTO_INCREMENT,
+  `delivery_type_name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`delivery_type_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Listage des données de la table ecom_literiedamour.delivery_type : ~2 rows (environ)
+DELETE FROM `delivery_type`;
+INSERT INTO `delivery_type` (`delivery_type_id`, `delivery_type_name`) VALUES
+	(1, 'A domicile'),
+	(2, 'Point de relais');
+	
+	
+ALTER TABLE customer_order
+ADD COLUMN delivery_type_id INT;
+ALTER TABLE customer_order
+ADD FOREIGN KEY (delivery_type_id) REFERENCES delivery_type(delivery_type_id);
+UPDATE customer_order
+SET delivery_type_id = 1;
+
+
+ALTER TABLE delivery_price 
+DROP FOREIGN KEY delivery_price_shop0_FK;
+ALTER TABLE delivery_price 
+DROP FOREIGN KEY delivery_price_wilaya_FK;
+ALTER TABLE delivery_price
+DROP PRIMARY KEY;
+
+ALTER TABLE delivery_price
+ADD COLUMN delivery_type_id INT;
+
+ALTER TABLE delivery_price
+ADD FOREIGN KEY (wilaya_id) REFERENCES wilaya(wilaya_id);
+ALTER TABLE delivery_price
+ADD FOREIGN KEY (shop_id) REFERENCES shop(shop_id);
+ALTER TABLE delivery_price
+ADD FOREIGN KEY (delivery_type_id) REFERENCES delivery_type(delivery_type_id);
+
+ALTER TABLE delivery_price
+ADD PRIMARY KEY (wilaya_id,shop_id,delivery_type_id);
+
+UPDATE delivery_price
+SET delivery_type_id = 1
+
+/* ------------------------------------add delivery type ------------------------------------*/
